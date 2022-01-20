779e6af7d7ce4ae4ce96c37b4a4a269e7c2deda3
meepma <80076185+meepma@users.noreply.github.com>
2022-01-20 18:45:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-video="";
+
 status1="";
+objects=[];
 
 
 function setup(){
@@ -12,6 +13,26 @@ function setup(){
 
 function draw(){
     image(video,0,0,480,380);
+    if(status1 !=""){
+        objectDetector.detect(video,gotResult);
+        for(i=0; i<objects.length;i++){
+            document.getElementById("Status").innerHTML="status: object detected";
+            fill("white");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("white");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(object_name==objects[i].label){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("ThingsDetected").innerHTML=object_name+" found";
+            }
+            else{
+                document.getElementById("ThingsDetected").innerHTML=object_name+" notfound";
+            }
+        }
+    }
 
 }
 
@@ -24,4 +45,13 @@ function start(){
 function modelLoaded(){
     console.log(modelLoaded);
     status1=true;
+    
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
 }
